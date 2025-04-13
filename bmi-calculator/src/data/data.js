bb935db54a57f766eafa9be4866eb69244423bda
JoaresMiranda <joaresmiranda@gmail.com>
2025-04-12 22:15:52 -0300

    file with data for bmi

---
+++ bmi-calculator/src/data/data.js
@@ -0,0 +1,39 @@
+export const data = [
+  {
+    min: 0,
+    max: 18.4,
+    classification: 'less than 18,5',
+    info: 'thinness',
+    obesity: 0,
+    infoClass: 'medium',
+  },
+  {
+    min: 18.5,
+    max: 24.9,
+    classification: 'between 18,5 and 24,9',
+    info: 'Normal',
+    obesity: 0,
+    infoClass: 'good',
+  },
+  {
+    min: 25,
+    max: 29.9,
+    classification: 'between 25,0 and 29,9',
+    obesity: 'I',
+    infoClass: 'Overweight',
+  },
+  {
+    min: 30,
+    max: 39.9,
+    classification: 'between 30,0 and 39,9',
+    obesity: 'II',
+    infoClass: 'Obesity',
+  },
+  {
+    min: 40,
+    max: 99,
+    classification: 'Greater than 40,0',
+    obesity: 'III',
+    infoClass: 'Severe obesity',
+  },
+];
